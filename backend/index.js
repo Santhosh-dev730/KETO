3c1b740db2f4d98d7e6e56fd64c9a51b4c8fb869
Santhosh-dev730 <120887367+Santhosh-dev730@users.noreply.github.com>
2025-07-16 17:00:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -50,7 +50,6 @@ app.get("/", (req, res) => {
 // MongoDB connection
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
 })
 .then(() => console.log(" MongoDB connected"))
 .catch((err) => console.error("MongoDB connection failed:", err));
