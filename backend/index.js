c72cb967991ce5d8bc56eb63c10e35d7d1489f5e
Santhosh-dev730 <sandysanthosh345@gmail.com>
2025-07-16 15:15:21 +0530

    Updated on index.js

--- backend/index.js
+++ backend/index.js
@@ -7,19 +7,34 @@ const userRoutes = require('./routes/userRoute');
 const adminRoutes = require('./routes/adminRoute');
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 7000;
+const MONGO_URI = process.env.MONGO_URL;
 
-app.use(cors({ origin: "http://localhost:5173", methods: ["GET", "POST", "PATCH", "DELETE"] }));
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(userRoutes);
 app.use(adminRoutes);
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Connected");
-    app.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => console.error("MongoDB Connection Failed:", err));
+
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("MongoDB Connected");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch((err) => {
+  console.error("MongoDB Connection Failed:", err);
+});
